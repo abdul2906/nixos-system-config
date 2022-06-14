93572972143248f6c91da63bd54d3c61bd4740c2
abdul2906 <abdul2906@tuta.io>
2022-06-14 13:59:20 +0200

    Replace tabs with spaces

--- nixos/user/abdul.nix
+++ nixos/user/abdul.nix
@@ -2,58 +2,58 @@
 { config, pkgs, ... }:
 
 {
-	users.users.abdul = {
+  users.users.abdul = {
     isNormalUser = true;
     description = "abdul";
     extraGroups = [ 
-    	"networkmanager" 
-    	"wheel" 
-    	"audio" 
-    	"video" 
-    	"docker" 
-    	];
-  	};
+      "networkmanager" 
+      "wheel" 
+      "audio" 
+      "video" 
+      "docker" 
+      ];
+    };
 
-  	# Session variables
-  	environment.sessionVariables = rec {
-    	EDITOR          = "nvim";
-    	NIXOS_OZONE_WL  = "1";
-  	};
+    # Session variables
+    environment.sessionVariables = rec {
+      EDITOR          = "nvim";
+      NIXOS_OZONE_WL  = "1";
+    };
 
-  	# Disable sudo password prompt
-  	security = {
-    	sudo.wheelNeedsPassword = false;
-  	};
+    # Disable sudo password prompt
+    security = {
+      sudo.wheelNeedsPassword = false;
+    };
 
-  	# Change default shell to fish
-  	programs.fish.enable = true;
-  	users.defaultUserShell = pkgs.fish;
+    # Change default shell to fish
+    programs.fish.enable = true;
+    users.defaultUserShell = pkgs.fish;
 
-  	# Change /bin/sh to dash
-  	environment.binsh = "${pkgs.dash}/bin/dash";
+    # Change /bin/sh to dash
+    environment.binsh = "${pkgs.dash}/bin/dash";
 
-  	# User applications
-  	environment.systemPackages = with pkgs; [
-  		# Personal applications
-  		neofetch
-  		discord
-  		neovim
-  		obsidian
-  		spotify
-  		btop
-  		nicotine-plus
-  		yt-dlp
-  		shotcut
-  		obs-studio
+    # User applications
+    environment.systemPackages = with pkgs; [
+      # Personal applications
+      neofetch
+      discord
+      neovim
+      obsidian
+      spotify
+      btop
+      nicotine-plus
+      yt-dlp
+      shotcut
+      obs-studio
 
-  		# Development
-  		git
-  		gcc
-  		nodejs
-  		nodePackages.npm
-  		rustc
-  		cargo
-  		jdk
-  		jre
-  	];
+      # Development
+      git
+      gcc
+      nodejs
+      nodePackages.npm
+      rustc
+      cargo
+      jdk
+      jre
+    ];
 }
